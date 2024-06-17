89a93552f10b5a2a9d135040ece9034855c11e8d
wldlkh <walid.elk@vivaldi.net>
2024-06-17 13:58:37 +0200

    placeholders

---
+++ app/formation/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Formation: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your Next.js TypeScript application.</p>
+    </div>
+  );
+}
+
+export default Formation;
---
+++ app/support/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your Next.js TypeScript application.</p>
+    </div>
+  );
+}
+
+export default Home;
