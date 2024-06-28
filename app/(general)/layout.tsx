97bef1bd5001d75710702ec58f4fcf5248067196
wldlkh <walid.elk@vivaldi.net>
2024-06-28 03:17:53 +0200

    FOOTER SIGNATURE

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import { MainNav } from "@/components/main-nav";
 import AuthNav from "@/components/auth-nav";
 import { checkPWA } from '@/components/pwa-checker';
+import Footer from "@/components/site-footer";
 
 
 export const metadata: Metadata = {
@@ -21,13 +22,14 @@ export default function RootLayout({
     <html lang="fr-FR">
       <body className="min-h-screen w-screen overflow-x-hidden font-sans antialiased">
         <Head />
-        <div className="relative flex min-h-screen flex-col">
+        <div className="relative flex flex-col min-h-screen">
           <div className="flex items-center space-x-4 sm:justify-between sm:space-x-0">
             <MainNav isPWA={checkPWA()}/>
           </div>
           <main className="flex-1">
             {children}
           </main>
+          <Footer />
         </div>
       </body>
     </html>
